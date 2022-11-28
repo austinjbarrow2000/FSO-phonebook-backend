65409e74d43c9eb2b22243c79e21a049025e92dd
Austin Barrow <austinjbarrow2000@gmail.com>
2022-11-28 01:20:25 -0800

    Add fly env port variable

--- index.js
+++ index.js
@@ -116,4 +116,5 @@ app.post('/api/persons', (request,response) => {
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+  console.log(process.env.PORT)
 })
