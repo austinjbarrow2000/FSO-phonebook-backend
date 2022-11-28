29ec7c1742e2f84036aecebdff0dcb9b6d3c9a67
Austin Barrow <austinjbarrow2000@gmail.com>
2022-11-28 01:04:07 -0800

    Add backend to fly

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 var morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
     return [
@@ -111,7 +113,7 @@ app.post('/api/persons', (request,response) => {
     
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
