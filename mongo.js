defbaee1273cb41efcb4d28c5a4fedb91fa36008
Austin Barrow <austinjbarrow2000@gmail.com>
2022-11-28 17:25:22 -0800

    Add mongodb and test drive with mongo.js

---
+++ mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://austinjbarrow2000:${password}@cluster0.71ftffl.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if( name === undefined || number === undefined ) {
+    mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      })
+  })
+  .catch((err) => console.log(err))
+} else {
+    mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    return person.save()
+  })
+  .then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    return mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
+}
